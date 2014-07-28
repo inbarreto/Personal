70e14745aeb785dc3969dd26e8d4b3d2cc184766
Ignacio <inbarreto86@gmail.com>
2014-07-28 02:12:37 -0300

    bug 1.3

---
+++ Personal/Personal.JsonAccess/JsonClasses/BuyJson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal.JsonAccess.JsonClasses
+{
+    public class BuyJson
+    {
+        public string session_id { get; set; }
+        public string device { get; set; }
+        public string element_id{ get; set; }
+        public string quality { get; set; }
+
+
+        public BuyJson()
+        {
+            session_id = string.Empty;
+            device = "windows_phone";
+            element_id = string.Empty;
+            quality = string.Empty;
+        }       
+
+    }
+}
---
+++ Personal/Personal/Views/Vistas.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Personal.JsonAccess.JsonClasses;
+using Newtonsoft.Json;
+using Personal.Domain.Entities;
+using Personal.Model;
+
+namespace Personal.Views
+{
+    public partial class Vistas : PhoneApplicationPage
+    {
+        public Vistas()
+        {
+            InitializeComponent();
+            this.Loaded += PeliculasFavoritos_Loaded;
+        }
+        void PeliculasFavoritos_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+
+                PeliculasPorGeneroJson peliPrincipal = new PeliculasPorGeneroJson();
+                peliPrincipal.session_id = ((Usuario)StateModel.ObtieneKey("Usuario")).session_id;
+                peliPrincipal.named_criteria = "visto";
+                string post_dataPeliculas = JsonConvert.SerializeObject(peliPrincipal);
+
+                peliculasVistas.CargaPeliculasPost(post_dataPeliculas, Personal.JsonAccess.URL.MenuCategoria);
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}

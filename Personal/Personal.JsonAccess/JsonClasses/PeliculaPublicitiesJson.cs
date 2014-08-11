e4b50c7c88d653cd76af658cef8327560c679f19
Ignacio <inbarreto86@gmail.com>
2014-08-11 13:20:58 -0300

    PeliculaPublicitiesJson

---
+++ Personal/Personal.JsonAccess/JsonClasses/PeliculaPublicitiesJson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal.JsonAccess.JsonClasses
+{
+    public class PeliculaPublicitiesJson
+    {
+      
+
+    public string device {get;set;}
+    public string @operator {get;set;}
+    public string session_id {get;set;}
+    public string content_id { get; set; }
+    public string ref_id { get; set; }
+
+    public PeliculaPublicitiesJson()
+        {
+            device = "windows_phone";
+            @operator = "qubit";
+            session_id = string.Empty;
+            content_id = string.Empty;
+            ref_id = string.Empty;
+        }
+
+    }
+}
